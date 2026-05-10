e34737936f7a186542db55ba375ac974472a4006
Martin Costa <info@marketingdeperformance.online>
2026-05-10 17:55:42 +0100

    chore: rename repository sitedown-styles to sitedown
    Per maintainer feedback on Jimmi08/e107-2.3.x-Lite#41 (avoid hyphen in repo name to prevent any future autoloading edge cases, and use a more generic name that allows broader sitedown-related functionality). GitHub creates an automatic permanent redirect from sitedown-styles, so no existing links break. Plugin folder name (sitedown_styles/) and visible plugin Name (Sitedown Styles) are intentionally preserved.

--- admin_config.php
+++ admin_config.php
@@ -187,12 +187,12 @@ class sitedown_styles_ui extends e_admin_ui
             'license'    => 'GNU General Public License v3.0',
             'license_url'=> 'https://www.gnu.org/licenses/gpl-3.0.html',
             'compat'     => 'e107 v2.3+ / PHP 8.0+',
-            'docs'       => 'https://github.com/e107-Kanonimpresor/sitedown-styles#readme',
-            'support'    => 'https://github.com/e107-Kanonimpresor/sitedown-styles/issues',
-            'repo'       => 'https://github.com/e107-Kanonimpresor/sitedown-styles',
-            'changelog'  => 'https://github.com/e107-Kanonimpresor/sitedown-styles/blob/main/CHANGELOG.md',
+            'docs'       => 'https://github.com/e107-Kanonimpresor/sitedown#readme',
+            'support'    => 'https://github.com/e107-Kanonimpresor/sitedown/issues',
+            'repo'       => 'https://github.com/e107-Kanonimpresor/sitedown',
+            'changelog'  => 'https://github.com/e107-Kanonimpresor/sitedown/blob/main/CHANGELOG.md',
             'donate'     => 'https://www.paypal.com/donate/?hosted_button_id=C2DA8NC5XP56A',
-            'review'     => 'https://github.com/e107-Kanonimpresor/sitedown-styles/releases',
+            'review'     => 'https://github.com/e107-Kanonimpresor/sitedown/releases',
         );
     }
 
